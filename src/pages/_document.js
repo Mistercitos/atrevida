b9a2cc76345097343cf950d67c6b00aa0bd67229
Christian Del Barco <cdelbarcog92@gmail.com>
2025-04-08 16:40:39 -0400

    resubida general

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="es">
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#ec1848" />
+        <meta
+          name="description"
+          content="Sitio oficial de ATREVIDA - Agencia Digital Chilena"
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
